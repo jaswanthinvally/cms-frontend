e797ce6c287e787b8d546c5b61b3797dc3224538
jaswanthinvally <jaswanthvenkat001@gmail.com>
2024-09-17 20:34:21 +0530

    login and signup page created

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Signup from './Signup'
+import Login from './Login'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className=' bg-sky-950 text-sky-500 h-screen flex justify-center items-center flex-col'>
+    <div className=''>
+    <div className=' flex flex-col justify-center'>
+    <h1 className='font-semibold text-2xl font-poppins '>welcome to next generation </h1>
+    <h2 className='font-semibold text-xl font-poppins text-center'>collage management system</h2>
+    </div>
+    <div className='flex justify-center mt-2'>
+      <Link to='/signup'><button className='bg-sky-500 text-white font-semibold p-2 rounded-md px-2 font-poppins mx-2'>signup</button></Link>
+      <Link to='/login'><button className='bg-sky-500 text-white font-semibold p-2 rounded-md px-2 font-poppins mx-2'>login</button></Link>
+    </div>
+    </div>
+    </div>
   )
 }
 
--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
+
 const Login = () => {
   return (
-    <div>Login</div>
-  )
+    <div className='bg-sky-950 h-screen font-poppins text-md font-semibold text-white flex flex-col justify-center items-center'>
+      <div className='bg-sky-500 rounded-xl w-auto p-3 flex flex-col'>
+        <h1 className='text-center text-xl'>Login</h1>
+        <label>username</label>
+        <input className=' rounded-md text-sky-950 focus:outline-none px-2' /><br />
+        <label>password</label>
+        <input className=' rounded-md text-sky-950 focus:outline-none px-2 '
+        type='password'
+         />
+         <button className='bg-sky-950 w-1/2 rounded p-1 mt-3'>submit</button>
+
+
+        
+      </div>
+    </div>
+      )
 }
 
-export default Login
+      export default Login
